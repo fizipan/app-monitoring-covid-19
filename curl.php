17563d34b73954b799f69264647b4bdabdb3b180
hafizhmaulanay <hafizhmy26@gmail.com>
2020-07-21 12:51:44 +0700

    update data berdasarkan provinsi

--- curl.php
+++ curl.php
@@ -13,9 +13,6 @@ function http_request($url)
     // aktifkan fungsi  transfer nilai yg berupa string
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-
     $output = curl_exec($ch);
 
     curl_close($ch);
